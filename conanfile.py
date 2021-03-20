8312ae9ace0147aa399784db15cec47cbbf81cd8
cele <cele83@hotmail.fr>
2021-03-20 21:41:10 +0100

    n

--- conanfile.py
+++ conanfile.py
@@ -2,31 +2,50 @@ from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 
 
+
 class CppTemplateConan(ConanFile):
     name = "CppTemplate"
     version = "0.0.1"
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
+    license = "MIT"
+    author = "Bertouz blopiblop100@gmail.com"
+    url = "https://github.com/Bertouz/CppTemplate"
     description = "<Description of CppTemplate here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    topics = ("c++", "template")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = {"shared": False}
+    options = {"shared": [True, False], "build_doc":[True, False], "build_test":[True, False], "use_conan":[True, False], "enable_code_coverage":[True, False], "enable_clang_tidy":[True, False], "enable_clang_format":[True, False] }
+    default_options = {"shared": False, "build_doc":False, "build_test":False, "use_conan":True, "enable_code_coverage":False, "enable_clang_tidy":False, "enable_clang_format":False}
     generators = "cmake_find_package"
-    requires="catch2/3.0.0@catchorg/stable"
+    requires= []
 
     def configure(self):
         if self.settings.compiler.cppstd not in ["11", "14","17"]:
             raise ConanInvalidConfiguration("C++ standard less than 11 not handled ")
 
+    def configure_cmake(self):
+        cmake = CMake(self)
+        kv = {True:"On", False:"Off"}
+        cmake.definitions["BUILD_DOC"]            = kv[self.options.build_doc is True]
+        cmake.definitions["BUILD_TESTS"]          = kv[self.options.build_test is True]
+        cmake.definitions["USE_CONAN"]            = kv[True] 
+        cmake.definitions["ENABLE_CODE_COVERAGE"] = kv[self.options.enable_code_coverage is True]
+        cmake.definitions["ENABLE_CLANG_TIDY"]    = kv[self.options.enable_clang_tidy is True]
+        cmake.definitions["ENABLE_CLANG_FORMAT"]  = kv[self.options.enable_clang_format is True]
+        cmake.configure()
+        return cmake
+
+    def requirements(self):
+        # Or add a new requirement!
+        if self.options.build_test is True:
+            self.requires("catch2/3.0.0@catchorg/stable")
+
     def source(self):
         self.run("".join(["git clone ",self.url]))
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure()
+        cmake = self.configure_cmake()
         cmake.build()
+        if self.options.build_test is True:
+            cmake.test()
 
         # Explicit way:
         # self.run('cmake %s/hello %s'
@@ -34,13 +53,9 @@ class CppTemplateConan(ConanFile):
         # self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        cmake = self.configure_cmake()
+        cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        self.cpp_info.libs = ["CppTemplate"]
 
