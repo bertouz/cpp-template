4264d998139fa5861e173d175eb573a4ddb1d46d
cele <cele83@hotmail.fr>
2021-03-20 19:21:03 +0100

    Config conan avec le generateur cmake_find_package

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
 
 
 class CppTemplateConan(ConanFile):
@@ -12,21 +13,19 @@ class CppTemplateConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = {"shared": False}
-    generators = "cmake"
+    generators = "cmake_find_package"
+    requires="catch2/3.0.0@catchorg/stable"
+
+    def configure(self):
+        if self.settings.compiler.cppstd not in ["11", "14","17"]:
+            raise ConanInvalidConfiguration("C++ standard less than 11 not handled ")
 
     def source(self):
-        self.run("git clone https://github.com/conan-io/hello.git")
-        # This small hack might be useful to guarantee proper /MT /MD linkage
-        # in MSVC if the packaged project doesn't have variables to set it
-        # properly
-        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(HelloWorld)",
-                              '''PROJECT(HelloWorld)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
+        self.run("".join(["git clone ",self.url]))
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="hello")
+        cmake.configure()
         cmake.build()
 
         # Explicit way:
