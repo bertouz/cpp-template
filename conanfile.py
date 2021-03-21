e0259f6c956b6f63af9f9f3abeb3378d84698bec
Bertouz <49638946+Bertouz@users.noreply.github.com>
2021-03-21 18:07:08 +0100

    Feature/conan (#11)
    Creation du conanfile.py de base
    Config conan avec le generateur cmake_find_package
    Add swap files to ignored files
    Update build.yml
    
    Co-authored-by: cele <cele83@hotmail.fr>
    Co-authored-by: bpeyce <bertrand.peyce@engie.com>

---
+++ conanfile.py
@@ -0,0 +1,66 @@
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+import os
+
+
+
+class CppTemplateConan(ConanFile):
+    name = "CppTemplate"
+    version = "0.0.1"
+    license = "MIT"
+    author = "Bertouz blopiblop100@gmail.com"
+    url = "https://github.com/Bertouz/CppTemplate"
+    description = "<Description of CppTemplate here>"
+    topics = ("c++", "template")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "build_doc":[True, False], "build_test":[True, False], "use_conan":[True, False], "enable_code_coverage":[True, False], "enable_clang_tidy":[True, False], "enable_clang_format":[True, False] }
+    default_options = {"shared": False, "build_doc":False, "build_test":False, "use_conan":True, "enable_code_coverage":False, "enable_clang_tidy":False, "enable_clang_format":False}
+    generators = "cmake_find_package"
+    requires= []
+    scm = { "type": "git",  # Use "type": "svn", if local repo is managed using SVN
+            "subfolder": "CppTemplate",
+            "url": "auto",
+            "revision": "auto",
+            "password": os.environ.get("SECRET", None)}
+
+    def configure(self):
+        if self.settings.compiler.cppstd not in ["11", "14","17"]:
+            raise ConanInvalidConfiguration("C++ standard less than 11 not handled ")
+
+    def configure_cmake(self):
+        cmake = CMake(self)
+        kv = {True:"On", False:"Off"}
+        cmake.definitions["BUILD_DOC"]            = kv[self.options.build_doc == True]
+        cmake.definitions["BUILD_TESTS"]          = kv[self.options.build_test == True]
+        cmake.definitions["USE_CONAN"]            = kv[True] 
+        cmake.definitions["ENABLE_CODE_COVERAGE"] = kv[self.options.enable_code_coverage == True]
+        cmake.definitions["ENABLE_CLANG_TIDY"]    = kv[self.options.enable_clang_tidy == True]
+        cmake.definitions["ENABLE_CLANG_FORMAT"]  = kv[self.options.enable_clang_format == True]
+        cmake.configure(source_folder="CppTemplate")
+        return cmake
+
+    def requirements(self):
+        if self.options.build_test == True:
+            self.requires("catch2/3.0.0@catchorg/stable")
+
+    #def source(self):
+        #self.run("".join(["git clone ",self.url]))
+
+    def build(self):
+        cmake = self.configure_cmake()
+        cmake.build()
+        if self.options.build_test is True:
+            cmake.test()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s'
+        #          % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        cmake = self.configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["CppTemplate"]
+
