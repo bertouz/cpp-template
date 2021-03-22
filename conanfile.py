633c8afe073e693a640624aa42a86738e4c1463b
Bertouz <49638946+Bertouz@users.noreply.github.com>
2021-03-22 23:19:06 +0100

    Feature/conan version git (#12)
    * change default values
    
    * a voir pour le create
    
    * update
    
    * v
    
    Co-authored-by: cele <cele83@hotmail.fr>

--- conanfile.py
+++ conanfile.py
@@ -10,18 +10,16 @@ class CppTemplateConan(ConanFile):
     license = "MIT"
     author = "Bertouz blopiblop100@gmail.com"
     url = "https://github.com/Bertouz/CppTemplate"
-    description = "<Description of CppTemplate here>"
+    description = ""
     topics = ("c++", "template")
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "build_doc":[True, False], "build_test":[True, False], "use_conan":[True, False], "enable_code_coverage":[True, False], "enable_clang_tidy":[True, False], "enable_clang_format":[True, False] }
     default_options = {"shared": False, "build_doc":False, "build_test":False, "use_conan":True, "enable_code_coverage":False, "enable_clang_tidy":False, "enable_clang_format":False}
     generators = "cmake_find_package"
     requires= []
-    scm = { "type": "git",  # Use "type": "svn", if local repo is managed using SVN
-            "subfolder": "CppTemplate",
-            "url": "auto",
-            "revision": "auto",
-            "password": os.environ.get("SECRET", None)}
+
+    def source(self):
+        self.run("".join(["git clone ", self.url]))
 
     def configure(self):
         if self.settings.compiler.cppstd not in ["11", "14","17"]:
@@ -43,24 +41,17 @@ class CppTemplateConan(ConanFile):
         if self.options.build_test == True:
             self.requires("catch2/3.0.0@catchorg/stable")
 
-    #def source(self):
-        #self.run("".join(["git clone ",self.url]))
-
     def build(self):
         cmake = self.configure_cmake()
         cmake.build()
         if self.options.build_test is True:
             cmake.test()
 
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
     def package(self):
         cmake = self.configure_cmake()
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["CppTemplate"]
+        self.cpp_info.libs    = ["libMyCppProject"]
+        self.cpp_info.libdirs = ["lib"]  # Directories where libraries can be found
 
