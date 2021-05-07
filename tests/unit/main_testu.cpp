1a45103b4ae5178584be4b42c6238b8cfa9c4764
bertouz <blopiblop100@gmail.com>
2021-05-07 19:38:14 +0200

    deplacement des includes publiques dans MyCppProject

--- tests/unit/main_testu.cpp
+++ tests/unit/main_testu.cpp
@@ -1,11 +1,8 @@
 #define CATCH_CONFIG_RUNNER
-#include "hello.hpp"
 #include <catch2/catch_session.hpp>
 
 int main(int argc, char *argv[])
 {
     const int res = Catch::Session().run(argc, argv);
-    my_cpp_project::PolitePerson person;
-    person.hello();
     return res;
 }
